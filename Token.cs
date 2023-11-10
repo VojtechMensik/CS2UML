8e180249ee5c998fed7525cab03508125bb29751
VojtechMensik <mensikv.03@spst.eu>
2023-11-10 09:28:21 +0100

    Create Token.cs

---
+++ Token.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS2UML
+{
+    internal class Token
+    {
+        public enum Type {identifier,keyword,operator_, delimiter };
+        public Type type;
+        public string
+        public Token()
+        {
+
+        }
+
+    }
+}
