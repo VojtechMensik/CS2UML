a1004b087c2f4fcaf11eac234d54db6a466c277c
VojtechMensik <mensikv.03@spst.eu>
2024-08-15 21:02:50 +0200

    Pokrok

--- CS2UML/Form1.Designer.cs
+++ CS2UML/Form1.Designer.cs
@@ -35,11 +35,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(272, 123);
+            this.button1.Location = new System.Drawing.Point(177, 124);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(95, 34);
+            this.button1.Size = new System.Drawing.Size(116, 69);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Převod Souboru";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -51,7 +51,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(510, 337);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
